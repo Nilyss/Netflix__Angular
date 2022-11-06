1850ca683633e8db46f1d2ac5878409e5c75b2e5
Nicolas DECRESSAC <101709161+Nilyss@users.noreply.github.com>
2022-11-06 08:06:09 +0100

    Display dynamic error in sign-up form
      - init a template-driven form in sign-up step1 component
      - Edit the password regular expression of back-end app, in user model to fit with front-end regex who's  according to auto generate password of firefox browser.

--- client/src/app/authentication/authentication.service.ts
+++ client/src/app/authentication/authentication.service.ts
@@ -1,24 +1,38 @@
 import { Injectable } from '@angular/core'
 import { HttpClient, HttpHeaders } from '@angular/common/http'
-import { Observable, tap, catchError } from 'rxjs'
+import { Observable, tap, catchError, of } from 'rxjs'
 import { User } from './user'
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
+  // CRUD users operations
   addUser(user: User): Observable<User> {
-    console.log('service proc =>')
-    console.log(user)
     return this.http
       .post<User>(this.UsersApiUrl + '/signup', user, this.httpOptions)
-      .pipe(tap((newUser) => console.log(`Create User ${newUser.email}`)))
+      .pipe(
+        tap((res) => this.log(res)),
+        catchError((error) => this.handleError(error, null))
+      )
+  }
+
+  // logs & errors
+  private log(res: any) {
+    console.table(res)
+  }
+
+  private handleError(error: Error, errorValue: any) {
+    console.error(error)
+    return of(errorValue)
+  }
+
+  // http request setup
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   }
 
   private UsersApiUrl: string = 'http://localhost:8000/api/users'
 
-  httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
-  }
   constructor(private http: HttpClient) {}
 }
