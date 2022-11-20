4c2c7327a8775d39aba7ff439240b7e77cb89140
Nicolas DECRESSAC <101709161+Nilyss@users.noreply.github.com>
2022-11-20 10:02:28 +0100

    Front-end: Implementation of env Script
     - Adding a script on init who's past the API key in .env file into environment.ts
     at server start.

--- client/src/app/videos/videos.service.ts
+++ client/src/app/videos/videos.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core'
+import { environment } from '../../environments/environment'
 import { HttpClient } from '@angular/common/http'
 import { Observable, tap, catchError, of, map } from 'rxjs'
 import { Video } from './video'
@@ -11,10 +12,12 @@ import { Trailer } from './trailer'
   providedIn: 'root',
 })
 export class VideosService {
+  apiKey = environment.THE_MOVIE_DB_API_KEY
+
   getVideosBySearch(query: string): Observable<Video['results']> {
     return this.http
       .get<Video['results']>(
-        `https://api.themoviedb.org/3/search/multi?api_key=e480c5de18e4c27893ea37f46f0af505&language=en-US&query=${query}&page=1&include_adult=false`
+        `https://api.themoviedb.org/3/search/multi?api_key=${this.apiKey}&language=en-US&query=${query}&page=1&include_adult=false`
       )
       .pipe(
         map((data) => data['results']),
@@ -28,7 +31,7 @@ export class VideosService {
   getTvShowById(tvShowId: string): Observable<TvShow> {
     return this.http
       .get<TvShow>(
-        `https://api.themoviedb.org/3/tv/${tvShowId}?api_key=e480c5de18e4c27893ea37f46f0af505&language=en-US`
+        `https://api.themoviedb.org/3/tv/${tvShowId}?api_key=${this.apiKey}&language=en-US`
       )
       .pipe(
         tap((res) => {
@@ -41,7 +44,7 @@ export class VideosService {
   getTvShowVideosById(tvShowId: string): Observable<Trailer> {
     return this.http
       .get<Trailer>(
-        ` https://api.themoviedb.org/3/tv/${tvShowId}/videos?api_key=e480c5de18e4c27893ea37f46f0af505&language=en-US`
+        ` https://api.themoviedb.org/3/tv/${tvShowId}/videos?api_key=${this.apiKey}&language=en-US`
       )
       .pipe(
         tap((res) => {
@@ -54,7 +57,7 @@ export class VideosService {
   getMovieById(movieId: string): Observable<Movie> {
     return this.http
       .get<Movie>(
-        ` https://api.themoviedb.org/3/movie/${movieId}?api_key=e480c5de18e4c27893ea37f46f0af505&language=en-US`
+        ` https://api.themoviedb.org/3/movie/${movieId}?api_key=${this.apiKey}&language=en-US`
       )
       .pipe(
         tap((res) => {
@@ -67,7 +70,7 @@ export class VideosService {
   getMovieVideosById(movieId: string): Observable<Trailer> {
     return this.http
       .get<Trailer>(
-        `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=e480c5de18e4c27893ea37f46f0af505&language=en-US`
+        `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${this.apiKey}&language=en-US`
       )
       .pipe(
         tap((res) => {
@@ -80,7 +83,7 @@ export class VideosService {
   getPersonById(personId: string): Observable<Person> {
     return this.http
       .get<Person>(
-        `https://api.themoviedb.org/3/person/${personId}?api_key=e480c5de18e4c27893ea37f46f0af505&language=en-US`
+        `https://api.themoviedb.org/3/person/${personId}?api_key=${this.apiKey}&language=en-US`
       )
       .pipe(
         tap((res) => {
